2445a56bf3f9e52041d8a1b6ff07baef95a2ade6
Wissam Charafeddine <wissam.charafeddine@gmail.com>
2016-03-30 16:46:31 -0400

    MARCH 30

--- lab4.js
+++ lab4.js
@@ -1,19 +1,19 @@
 var temp;
 var type;
 function convertor (temp, type) {
-	// type = type.tolowercase();
-  //if (type="F")||
-  if (type="f") {
-    temp= (temp-32)*(5/9);
-    console.log(temp+" "+type+" degrees!");
+	// type = type.toLowerCase();
+  //if (type==="F")||
+  if (type==="f") {
+    ntemp= (temp-32)*(5/9);
+    console.log(temp+" degrees in F is equal to " +ntemp+" degrees in C");
   }
-  //else (type="C")||
-  else if (type="c") {
-    temp = (temp *(9/5))+32;
-    console.log(temp+" "+type+" degrees!");
+  //else (type==="C")||
+  else if (type==="c") {
+    ntemp = (temp *(9/5))+32;
+    console.log(temp+" degrees in C is equal to " +ntemp+" degrees in F");
   }
   else {
     console.log("TYPO IDIOT!!!!!")
   }
 }
-console.log(convertor(212, "c"));
+console.log(convertor(100, "c"));
